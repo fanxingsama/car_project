3bcf30ec981fd9966d87d1f22d3852d25683564b
fanxingsama <76940503+fanxingsama@users.noreply.github.com>
2024-01-11 22:12:57 +0800

    Add files via upload

---
+++ car_project/car_web/src/main/org/example/entiyies/web_user.java
@@ -0,0 +1,12 @@
+package org.example.entiyies;
+
+/**
+ * @author 马云龙
+ * @create 2024-01-06 15:01
+ */
+public class web_user {
+    private Integer id;
+    private String username;
+    private String password;
+
+}
